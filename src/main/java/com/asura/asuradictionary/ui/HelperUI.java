f3bd4c51556971b85cc5d2de3c0cc6a0a316ae17
Asuraman <manhpdbiz@gmail.com>
2023-09-28 23:18:07 +0700

    create project base

---
+++ src/main/java/com/asura/asuradictionary/ui/HelperUI.java
@@ -0,0 +1,40 @@
+package com.asura.asuradictionary.ui;
+
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class HelperUI {
+    static final FileChooser fileChooser = new FileChooser();
+    static final DirectoryChooser dirChooser = new DirectoryChooser();
+
+    /**
+     * Use FileChooser to open a popup window to choose the file.
+     *
+     * @param stage the stage to open the FileChooser from
+     * @return path of the chosen file
+     */
+    public static String chooseFile(Stage stage) {
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            return file.getAbsolutePath();
+        }
+        return "";
+    }
+
+    /**
+     * Use DirectoryChooser to open a popup window to choose the directory.
+     *
+     * @param stage the stage to open the DirectoryChooser from
+     * @return path of the chosen directory
+     */
+    public static String chooseDir(Stage stage) {
+        File dir = dirChooser.showDialog(stage);
+        if (dir != null) {
+            return dir.getAbsolutePath();
+        }
+        return "";
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.asura.asuradictionary {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
+    requires jlayer;
+    requires javafx.web;
+
     opens com.asura.asuradictionary to javafx.fxml;
     exports com.asura.asuradictionary;
 }
