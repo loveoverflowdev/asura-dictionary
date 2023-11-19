b4d89798a35ce05aa34a6f2b4ff06cdb12537985
Asuraman <manhpdbiz@gmail.com>
2023-11-20 02:28:58 +0700

    add guess word base game

---
+++ src/main/java/com/asura/guessword/game/Controller.java
@@ -0,0 +1,22 @@
+package com.asura.guessword.game;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class Controller {
+    public void changeScene(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/gameScene.fxml"));
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setTitle("Hangman Game");
+        window.setScene(new Scene(parent, 800, 650));
+        window.show();
+    }
+
+}
